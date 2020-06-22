4794578d14b8861c76f215a80884f34cc29d1f3b
Michael P Nance <michael.p.nance@gmail.com>
2020-06-22 03:06:16 -0400

    Added routes to get data from game atlas API

---
+++ routes/atlas-routes.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Dependencies
+const fetch = require("node-fetch");
+const key = require("../config/keys");
+
+// Useful constants
+const endpoint = "https://www.boardgameatlas.com/api";
+
+
+
+// Function defs
+const apiSearch = async (title) =>
+    (await fetch(`${endpoint}/search?name=${title}&client_id=${key.atlasID}`, { method: "Get" })).json();
+
+module.exports = app => {
+    // Returns list of games matching given string
+    app.get("/atlas/list", async (req, res) => {
+        res.json((await apiSearch(req.body.title)).games.map(x => x.name));
+    });
+
+    // Returns detailed information for game with given title
+    app.get("/atlas/info", async (req, res) => {
+        res.json((await apiSearch(req.body.title)).games.filter(x => x.name === req.body.title));
+    });
+};
